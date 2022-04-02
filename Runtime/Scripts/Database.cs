8823dc162da3feaa2f8fdfa1793b1ac573f11c1b
Jay Richardson <jay@werkbot.com>
2022-04-02 11:55:02 -0400

    Added namespace

--- Runtime/Scripts/Database.cs
+++ Runtime/Scripts/Database.cs
@@ -5,47 +5,48 @@ using System;
 using System.IO;
 using Mono.Data.Sqlite;
 
-public class Database {
+namespace Aletail.General {
+    public class Database {
 
-    public string DatabaseFileName = "";
+        public string DatabaseFileName = "";
 
-    IDbConnection dbconn;
-    IDbCommand dbcmd;
-    IDataReader reader;
+        IDbConnection dbconn;
+        IDbCommand dbcmd;
+        IDataReader reader;
 
-    /*
-    * Database Constructors
-    */
-    public Database() { }
+        /*
+        * Database Constructors
+        */
+        public Database() { }
 
-    /*
-    * Database
-    */
-    public Database(string SaveFileName)
-    {
-        this.DatabaseFileName = SaveFileName;
-        this.OpenDatabase(SaveFileName);
-    }
-
-    /*
-    * Open Database
-    */
-    public void OpenDatabase(string SaveFileName = "", Boolean Stream = false)
-    {
-
-        if (this.DatabaseFileName == "")
+        /*
+        * Database
+        */
+        public Database(string SaveFileName)
         {
             this.DatabaseFileName = SaveFileName;
-            Debug.Log("Database Name Set: " + this.DatabaseFileName);
+            this.OpenDatabase(SaveFileName);
         }
 
-        // The following block of code for locating the referenced database file was taken from somewhere it is probably out of date. If you know the original author please contact
-        // https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html
-        string filepath = Application.persistentDataPath + "/" + this.DatabaseFileName;
-        if (!File.Exists(filepath))
+        /*
+        * Open Database
+        */
+        public void OpenDatabase(string SaveFileName = "", Boolean Stream = false)
         {
-            if (Stream)
+
+            if (this.DatabaseFileName == "")
             {
+                this.DatabaseFileName = SaveFileName;
+                Debug.Log("Database Name Set: " + this.DatabaseFileName);
+            }
+
+            // The following block of code for locating the referenced database file was taken from somewhere it is probably out of date. If you know the original author please contact
+            // https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html
+            string filepath = Application.persistentDataPath + "/" + this.DatabaseFileName;
+            if (!File.Exists(filepath))
+            {
+                if (Stream)
+                {
 #if UNITY_ANDROID
                 //COPY EXISTING
                 var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + this.DatabaseFileName);  // this is the path to your StreamingAssets in android
@@ -58,9 +59,9 @@ public class Database {
                 string _loaddb = Application.streamingAssetsPath + "/" + this.DatabaseFileName;
                 File.Copy(_loaddb, filepath, true);
 #endif
-            }
-            else
-            {
+                }
+                else
+                {
 #if UNITY_ANDROID
                 //COPY EXISTING
                 var loadDb = new WWW("jar:file://" + Application.persistentDataPath + "!/assets/" + this.DatabaseFileName);  // this is the path to your StreamingAssets in android
@@ -68,43 +69,44 @@ public class Database {
                                             // then save to Application.persistentDataPath
                 File.WriteAllBytes(filepath, loadDb.bytes);
 #endif
+                }
             }
+            String conn = "URI=file:" + filepath;
+            dbconn = new SqliteConnection(conn);
+            dbconn.Open();
+
         }
-        String conn = "URI=file:" + filepath;
-        dbconn = new SqliteConnection(conn);
-        dbconn.Open();
 
-    }
-
-    /*
-    * Execute Query
-    */
-    public IDataReader ExecuteQuery(string sqlQuery)
-    {
-        dbcmd = dbconn.CreateCommand();
-        dbcmd.CommandText = sqlQuery;
-        reader = dbcmd.ExecuteReader();
-        return reader;
-    }
-
-
-    /*
-    * Close Database
-    */
-    public void CloseDatabase()
-    {
-        if (reader != null)
+        /*
+        * Execute Query
+        */
+        public IDataReader ExecuteQuery(string sqlQuery)
         {
-            reader.Close();
-            reader = null;
+            dbcmd = dbconn.CreateCommand();
+            dbcmd.CommandText = sqlQuery;
+            reader = dbcmd.ExecuteReader();
+            return reader;
         }
-        if (dbcmd != null)
-        {
-            dbcmd.Dispose();
-            dbcmd = null;
-        }
-        dbconn.Close();
-        dbconn = null;
-    }
 
+
+        /*
+        * Close Database
+        */
+        public void CloseDatabase()
+        {
+            if (reader != null)
+            {
+                reader.Close();
+                reader = null;
+            }
+            if (dbcmd != null)
+            {
+                dbcmd.Dispose();
+                dbcmd = null;
+            }
+            dbconn.Close();
+            dbconn = null;
+        }
+
+    }
 }
